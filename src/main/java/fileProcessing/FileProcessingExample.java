e99262e8d021bf0dd828ec094a2a2b62d5eaa32a
Olga Karpenko <okarpenko@usfca.edu>
2023-08-29 08:54:13 -0700

    Added file processing examples.

---
+++ src/main/java/fileProcessing/FileProcessingExample.java
@@ -0,0 +1,23 @@
+package fileProcessing;
+
+import java.io.File;
+
+// Older way to iterate over files in a given directory. Use DirectoryStream instead - see DirectoryStreamExample
+public class FileProcessingExample {
+    /** Prints the names of all files in a given directory (on the top level,
+     * without going into subdirectories
+     * @param directory directory where the method searches for files
+     */
+    public static void processFiles(String directory) {
+        File dir = new File(directory);
+        for (File file: dir.listFiles()) {
+            if (file.toString().endsWith(".java"))
+                System.out.println(file);
+        }
+    }
+
+    public static void main(String[] args) {
+        processFiles("src/main/java/dataStructures");
+    }
+
+    }
