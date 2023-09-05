bae84fb4b9e5ea15797caad6c398baf7634c0327
Olga Karpenko <okarpenko@usfca.edu>
2023-09-05 09:36:28 -0700

    Added a date formatting example.

---
+++ src/main/java/javaBasics/DateFormattingExample.java
@@ -0,0 +1,14 @@
+package javaBasics;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormattingExample {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String date = "2022-06-09";
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        System.out.println(localDate);
+        System.out.println(localDate.getDayOfWeek());
+    }
+}
