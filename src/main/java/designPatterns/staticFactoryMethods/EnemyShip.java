e88ee1cb36950e85ab246507fc3476c0a74ee35a
Olga Karpenko <okarpenko@usfca.edu>
2023-09-14 08:43:01 -0700

    Static factory methods.

---
+++ src/main/java/designPatterns/staticFactoryMethods/EnemyShip.java
@@ -0,0 +1,25 @@
+package designPatterns.staticFactoryMethods;
+
+import java.lang.reflect.InvocationTargetException;
+
+public interface EnemyShip {
+    void attack();
+    static EnemyShip getInstance(String type) {
+        /*if (type.endsWith("UFOShip"))
+            return new UFOShip();
+        else if (type.endsWith("PirateShip"))
+                return new PirateShip();
+        return null; */
+        try {
+            Class cl = Class.forName(type);
+            Object obj = cl.getDeclaredConstructor().newInstance();
+            EnemyShip ship = (EnemyShip) obj;
+            return ship;
+
+        } catch (ClassNotFoundException | NoSuchMethodException| InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            System.out.println(e);
+        }
+        return null;
+
+       }
+}
