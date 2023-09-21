8f262adad53fee6f7959a9c1851e1f23f69e7c7b
Olga Karpenko <okarpenko@usfca.edu>
2023-09-21 08:53:23 -0700

    Updated the ShapesTest.

--- src/main/java/designPatterns/adapter/ShapesTest.java
+++ src/main/java/designPatterns/adapter/ShapesTest.java
@@ -7,24 +7,22 @@ public class ShapesTest {
 		int x2 = 30, y2 = 60;
 
 		// Without the adapter: not elegant
-		/*
 		Object[] shapes = { new Line(), new Rectangle() };
 		for (int i = 0; i < shapes.length; ++i)
 			if (shapes[i] instanceof Line)
-				((Line) shapes[i]).draw(x1, y1, x2, y2);
+				((Line) shapes[i]).drawLine(x1, y1, x2, y2);
 			else if (shapes[i] instanceof Rectangle)
-				((Rectangle) shapes[i]).draw(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x2 - x1), Math.abs(y2 - y1));
-		*/
+				((Rectangle) shapes[i]).drawRec(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x2 - x1), Math.abs(y2 - y1));
 		
 		// After using adapter: clean, general, elegant
 
-		Shape[] shapesArr = { new LineShape(), new RectangleShape() };
+		/*Shape[] shapesArr = { new LineShape(), new RectangleShape() };
 		// A beginning and end point from a graphical editor
 		int px1 = 10, py1 = 20;
 		int px2 = 30, py2 = 60;
 		for (int i = 0; i < shapesArr.length; ++i)
 			shapesArr[i].draw(x1, y1, x2, y2);
-
+		*/
 	}
 
 }
